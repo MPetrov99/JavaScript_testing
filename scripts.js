08d788527e9f21e151dfce7b8f6468c75194d7bb
MPetrov99 <98600216+MPetrov99@users.noreply.github.com>
2022-03-09 18:37:23 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,140 @@
+// Lesson 1
+
+//let surname = prompt('Greetings friend, may I enquire as to your surname?');
+
+// let lastName;
+// let age;
+
+// let name = 'John';
+// let age2 = 20;
+
+//Lesson 2
+
+// let apples = 5,
+//     pears = 10;
+
+// let combinedFruitNumber = apples + pears;
+
+// let splitFruits = combinedFruitNumber / 3;
+
+// Lesson 3
+
+// let num1 = 5.12,
+//     num2 = 5.12,
+//     num3 = 6.13;
+
+//Lesson 4
+
+// if (num1 < num3) {
+//     "Number 1 is less than Number 3"
+// };
+
+// if (num3 < num1) {
+//     "Number 3 is less than Number 1"
+// } else {
+//     "Number 3 is not less than Number 1"
+// };
+
+// Lesson 5
+
+// let i = 1
+
+// while (i < 11) {
+//     alert(i);
+
+//     i = i + 1;
+// };
+
+// for (let p = 1; p < 10; p++) {
+//     alert(p);
+// };
+
+// for (let p = 1; p < 10; p += 2) {
+//     alert(p);
+// };
+
+// Lesson 6
+
+// let a = 2,
+//     b = 3;
+
+// let combine = function(a, b) {
+//     return a + b;
+// }
+
+// Lesson 7 
+
+// let jedi = {
+//     name: 'Yoda',
+//     years: 900,
+//     talk: function() { alert("another... Sky... walk..."); }
+// }
+
+// jedi.lightsaber = {
+//     color: "Green",
+//     quality: "Rare"
+// }
+
+// let dog = {};
+
+// dog.bark = function() { alert("Woof"); }
+
+// Lesson 8
+
+// let box1 = [];
+// let box2 = ["cireal", "milk", "sweets"];
+
+// box2.push('chocolate');
+
+// box2.pop();
+
+// let helloFrom = function(personName) {
+//     return "Hello From " + personName
+// }
+
+// let people = ["Tom", "Yoda", "John"];
+
+// people.push("Dr. Black");
+// people.push("Smith");
+
+// people.pop();
+
+// for (let i = 0; i < people.length; i++) {
+//     let greetings = helloFrom(people[i]);
+//     alert(greetings);
+// }
+
+// Lesson 9
+
+// let handleClick = function(event) {
+//     alert("I was clicked");
+// }
+
+// let button = document.getElementsByClassName('btn1');
+// button.addEventListener('click', handleClick);
+
+// let handleClick = function(event) {
+//     // do something!
+// };
+// let button = document.querySelector('btn1');
+// button.addEventListener('click', handleClick);
+
+// Lesson 10
+
+// let req = new XMLHttpRequest();
+// req.onload = function(event) {};
+// req.open('get', 'some-file.txt', true);
+// req.send();
+
+// Lesson 11
+
+// var currentHeight = $('.note').height(),
+//     currentColor = $('.note').css('color');
+
+// Lesson 12 
+
+// let Person = function(name) {
+//     this.name = name;
+// }
+
+// Lesson 13
